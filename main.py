abe2064a2aa94be1ec7a66798fc1dd023585dfb0
DarkProrok <Seroshtanmakcs@gmail.com>
2024-03-27 16:51:43 +1000

    add logger and fault-tolerant bot.polling

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+import time
+
 import requests
 from database import Session
 from bot_init import bot
-from admin import handler_admin_all_intervals, handle_admin_choice, handle_admin_choice_late, handle_admin_choice_ready, handle_admin_choice_dicline
+from admin import (handler_admin_all_intervals, handle_admin_choice, handle_admin_choice_late,
+                   handle_admin_choice_ready, handle_admin_choice_dicline)
 from handlers import handle_day, send_keyboard, handle_button_click, handle_my_intervals, handle_button_click_decline, \
     handle_start, handle_button_accept
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 @bot.message_handler(commands=["new"])
 def handle_day_wrapper(message):
-    with Session() as session:
-        handle_day(message, session)
+    handle_day()
 
 
 @bot.message_handler(commands=['start'])
 def handle_start_wrapper(message):
+    logging.info(f"Пользователь {message.from_user.username} активировал бота")
     handle_start(message)
 
 
@@ -41,7 +47,7 @@ def handle_button_click_decline_wrapper(call):
 @bot.callback_query_handler(func=lambda call: call.data.startswith('accept_'))
 def handle_button_accept_wrapper(call):
     with Session() as session:
-        handle_button_accept(call, session)
+        handle_button_accept(call)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('choice_'))
@@ -61,21 +67,24 @@ def handle_button_accept_wrapper(call):
 
 @bot.message_handler(commands=['my'])
 def handle_my_intervals_wrapper(message):
+    logging.info(f"Пользователь {message.from_user.username} посмотрел активные интервалы")
     handle_my_intervals(message=message)
 
 
 @bot.message_handler(commands=['today'])
 def handler_admin_all_intervals_wrapper(message):
+    logging.info(f"Пользователь {message.from_user.username} посмотрел активные за весь день")
     handler_admin_all_intervals(message)
 
 
-while True:
+def start_bot():
     try:
+        print("Бот работает!")
         bot.polling(none_stop=True)
-        exit()  # для остановки бота через ctrl+c, не удалять!
-    except requests.exceptions.ReadTimeout as tmp:
-        print(str(tmp))
-    except requests.exceptions.SSLError as tmp:
-        print(str(tmp))
-    except requests.exceptions.ConnectionError as tmp:  # Max retries exceeded with url
-        print(str(tmp))
+    except Exception as e:
+        logging.info(f"Произошла ошибка {e}")
+        time.sleep(5)
+        start_bot()
+
+
+start_bot()
